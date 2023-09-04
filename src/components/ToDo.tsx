5b476587499c7dba987ec1e552bc141735bbc029
mijinB <bmj13465@gmail.com>
2023-09-04 18:39:46 +0900

    Enums 사용해서 category 오타 위험 제거

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,5 +1,5 @@
 import { useSetRecoilState } from "recoil";
-import { IToDo, toDoState } from "../atoms";
+import { Categories, IToDo, toDoState } from "../atoms";
 
 function ToDo({ id, text, category }: IToDo) {
     const setToDos = useSetRecoilState(toDoState);
@@ -26,9 +26,9 @@ function ToDo({ id, text, category }: IToDo) {
     return (
         <li>
             <span>{text}</span>
-            {category !== "TO_DO" && <button onClick={() => onClick("TO_DO")}>To Do</button>}
-            {category !== "DOING" && <button onClick={() => onClick("DOING")}>Doing</button>}
-            {category !== "DONE" && <button onClick={() => onClick("DONE")}>Done</button>}
+            {category !== Categories.TO_DO && <button onClick={() => onClick(Categories.TO_DO)}>To Do</button>}
+            {category !== Categories.DOING && <button onClick={() => onClick(Categories.DOING)}>Doing</button>}
+            {category !== Categories.DONE && <button onClick={() => onClick(Categories.DONE)}>Done</button>}
         </li>
     );
 }

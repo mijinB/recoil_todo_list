46b1e9c906348f540c2b66d5041514d45342bfc2
mijinB <bmj13465@gmail.com>
2023-09-04 16:00:05 +0900

    category버튼 클릭 시, 수정하기 위한 get data

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,7 +1,49 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text }: IToDo) {
-    return <li>{text}</li>;
+function ToDo({ id, text, category }: IToDo) {
+    const setToDos = useSetRecoilState(toDoState);
+
+    /**@function onClick
+     * 1. MouseEvent를 인자로 받아서 수정할 category를 변수(name)에 대입
+     * 2. category를 수정하고자 하는 toDo의 index 찾아서 변수(targetIndex)에 대입
+     * 3. targetIndex의 index 값을 사용해서 수정하고자 하는 toDo의 object를 변수(oldToDo)에 대입
+     * 4. 컴포넌트 props로 받은 id, text는 그 값을 유지하고 category는 수정될 값(name)으로 변수(newToDo)에 대입
+     */
+    const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const {
+            currentTarget: { name },
+        } = event;
+
+        setToDos((oldToDos) => {
+            const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+            const oldToDo = oldToDos[targetIndex];
+            const newToDo = { id, text, category: name };
+            console.log(oldToDo, newToDo);
+            return oldToDos;
+        });
+    };
+
+    return (
+        <li>
+            <span>{text}</span>
+            {category !== "TO_DO" && (
+                <button name="TO_DO" onClick={onClick}>
+                    To Do
+                </button>
+            )}
+            {category !== "DOING" && (
+                <button name="DOING" onClick={onClick}>
+                    Doing
+                </button>
+            )}
+            {category !== "DONE" && (
+                <button name="DONE" onClick={onClick}>
+                    Done
+                </button>
+            )}
+        </li>
+    );
 }
 
 export default ToDo;

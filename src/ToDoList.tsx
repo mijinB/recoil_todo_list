6058ad9402d8669eff3595f2e75bc4b8569c155e
mijinB <bmj13465@gmail.com>
2023-09-04 10:03:17 +0900

    react-hook-form 설치 useForm 사용법

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,36 +1,37 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-function TodoList() {
+/* function TodoList() {
     const [todo, setTodo] = useState("");
-    const [todoError, setTodoError] = useState("");
+    const [todoError, setTodoError] = useState(""); */
 
-    /**@function onChange
-     * 1. setTodo 사용해서 todo input 값으로 변경
-     * 2. setTodoError 사용해서 todoError 초기화
-     */
-    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+/**@function onChange
+ * 1. setTodo 사용해서 todo input 값으로 변경
+ * 2. setTodoError 사용해서 todoError 초기화
+ */
+/*     const onChange = (event: React.FormEvent<HTMLInputElement>) => {
         const {
             currentTarget: { value },
         } = event;
 
         setTodoError("");
         setTodo(value);
-    };
+    }; */
 
-    /**@function onSubmit
-     * 1. <from>의 submit 기본 동작(reLoad) 막기
-     * 2. 입력된 todo가 10글자 미만이면 todoError에 "To do should be longer" 대입
-     * 3. 10글자 이상이면 "submit" 출력
-     */
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+/**@function onSubmit
+ * 1. <from>의 submit 기본 동작(reLoad) 막기
+ * 2. 입력된 todo가 10글자 미만이면 todoError에 "To do should be longer" 대입
+ * 3. 10글자 이상이면 "submit" 출력
+ */
+/*     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         if (todo.length < 10) {
             return setTodoError("To do should be longer");
         }
         console.log("submit");
-    };
+    }; */
 
-    return (
+/*     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input value={todo} onChange={onChange} placeholder="Write a to do" />
@@ -39,6 +40,24 @@ function TodoList() {
             </form>
         </div>
     );
+} */
+
+function ToDoList() {
+    const { register, watch } = useForm();
+    console.log(watch());
+
+    return (
+        <div>
+            <form>
+                <input {...register("email")} placeholder="Email" />
+                <input {...register("firstName")} placeholder="First Name" />
+                <input {...register("lastName")} placeholder="Last Name" />
+                <input {...register("username")} placeholder="Username" />
+                <input {...register("password")} placeholder="Password" />
+                <button>Add</button>
+            </form>
+        </div>
+    );
 }
 
-export default TodoList;
+export default ToDoList;

d07e050b2277fd0ea72710a6e5c8623cf42ff2f7
mijinB <bmj13465@gmail.com>
2023-09-04 09:50:41 +0900

    기본 react form 조건 검사

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,24 +2,32 @@ import { useState } from "react";
 
 function TodoList() {
     const [todo, setTodo] = useState("");
+    const [todoError, setTodoError] = useState("");
 
     /**@function onChange
      * 1. setTodo 사용해서 todo input 값으로 변경
+     * 2. setTodoError 사용해서 todoError 초기화
      */
     const onChange = (event: React.FormEvent<HTMLInputElement>) => {
         const {
             currentTarget: { value },
         } = event;
+
+        setTodoError("");
         setTodo(value);
     };
 
     /**@function onSubmit
      * 1. <from>의 submit 기본 동작(reLoad) 막기
-     * 2. 확인을 위한 출력문
+     * 2. 입력된 todo가 10글자 미만이면 todoError에 "To do should be longer" 대입
+     * 3. 10글자 이상이면 "submit" 출력
      */
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log(todo);
+        if (todo.length < 10) {
+            return setTodoError("To do should be longer");
+        }
+        console.log("submit");
     };
 
     return (
@@ -27,6 +35,7 @@ function TodoList() {
             <form onSubmit={onSubmit}>
                 <input value={todo} onChange={onChange} placeholder="Write a to do" />
                 <button>Add</button>
+                {todoError ?? todoError}
             </form>
         </div>
     );
